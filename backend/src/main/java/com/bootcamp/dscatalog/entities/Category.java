d7ae938ce991e43f056386146d62ff14a6980a2e
Brendo Silva <breendo.01@gmail.com>
2022-02-09 10:09:07 -0300

    add database h2

--- backend/src/main/java/com/bootcamp/dscatalog/entities/Category.java
+++ backend/src/main/java/com/bootcamp/dscatalog/entities/Category.java
@@ -3,16 +3,24 @@ package com.bootcamp.dscatalog.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class Category implements Serializable {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name= "tb_category")
+public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
 	private Long id;
-	private String name;
 	
-	public Category(Long id, String name ) {
-		this.id = id;
-		this.name = name;
-	}
+	@Column
+	private String name;	
 
 	
 	public Long getId( ) {
@@ -42,7 +50,6 @@ public class Category implements Serializable {
 		Category other = (Category) obj;
 		return Objects.equals(id, other.id) && Objects.equals(name, other.name);
 	}
-	
 	
 
 }
