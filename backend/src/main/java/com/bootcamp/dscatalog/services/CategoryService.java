d7c5f33d24611140903abbf9a9a5effb13b62cf0
Brendo Silva <breendo.01@gmail.com>
2022-02-13 19:11:12 -0300

    create find by id, exception Handle

--- backend/src/main/java/com/bootcamp/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/bootcamp/dscatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.bootcamp.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.bootcamp.dscatalog.DTO.CategoryDTO;
 import com.bootcamp.dscatalog.entities.Category;
 import com.bootcamp.dscatalog.repositories.CategoryRepository;
+import com.bootcamp.dscatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -27,4 +29,13 @@ public class CategoryService {
 		
 		return listCategoryDto;
 	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = categoryRepository.findById(id);
+		Category categoryEntitie = obj.orElseThrow(() -> new EntityNotFoundException("Category not found"));
+		return new CategoryDTO(categoryEntitie);
+	}
+	
+	
 }
