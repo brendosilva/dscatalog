02c4a83d1ec2b908f81db6515ece580fec0efcc3
Brendo Silva <breendo.01@gmail.com>
2022-02-18 09:04:34 -0300

    Auditing data

--- backend/src/main/java/com/bootcamp/dscatalog/entities/Category.java
+++ backend/src/main/java/com/bootcamp/dscatalog/entities/Category.java
@@ -1,6 +1,7 @@
 package com.bootcamp.dscatalog.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.Objects;
 
 import javax.persistence.Column;
@@ -8,19 +9,28 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 @Entity
 @Table(name= "tb_category")
 public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
+	
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)	
 	private Long id;
 	
 	@Column
-	private String name;	
+	private String name;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt;
 
 	
 	public Long getId( ) {
@@ -34,7 +44,26 @@ public class Category implements Serializable {
 	}
 	public void setName(String name) {
 		this.name = name;
+	}	
+	
+	public Instant getCreatedAt() {
+		return createdAt;
 	}
+	
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+	
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		updatedAt = Instant.now();
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id, name);
